4ccc12c534a57d47c29142e58a2430c736773a28
Sascha Dehler <saschadehler@gmail.com>
2018-08-13 09:59:11 +0200

    feat: Created BookShelf component

---
+++ src/BookShelf.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import BookList from './BookList';
+import PropTypes from 'prop-types';
+
+class BookShelf extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired
+  }
+
+  render() {
+    const title = this.props.title;
+    const books = this.props.books;
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <BookList books={books} />
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default BookShelf;
