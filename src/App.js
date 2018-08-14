db66517fea5da2236e49bc8bc65c3ac68475939b
Sascha Dehler <saschadehler@gmail.com>
2018-08-14 10:03:02 +0200

    feat: Included BookSearch component in APP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BookShelf from './BookShelf';
+import BookSearch from './BookSearch';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
@@ -25,26 +26,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <BookSearch getBookFromShelf={(bookID) => {this.getBookFromShelf(bookID)}}/>
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -66,6 +48,10 @@ class BooksApp extends React.Component {
 
   filterBooksByShelf = (shelf) => {
     return this.state.books.filter((book) => book.shelf === shelf);
+  }
+
+  getBookFromShelf = (bookID) => {
+
   }
 }
 
